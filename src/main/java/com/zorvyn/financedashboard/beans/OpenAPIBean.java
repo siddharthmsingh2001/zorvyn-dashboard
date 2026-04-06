89de791b19fc00a5de0d193000326648924e6498
siddharthmsingh2001 <siddharthmsingh2001@gmail.com>
2026-04-06 22:57:18 +0530

    Added the OpenApi Config

---
+++ src/main/java/com/zorvyn/financedashboard/beans/OpenAPIBean.java
@@ -0,0 +1,31 @@
+package com.zorvyn.financedashboard.beans;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Zorvyn Finance Log API",
+                version = "1.0",
+                description = "Personalized Finance Tracking System with RBAC",
+                contact = @Contact(name = "Siddharth", email = "siddhartmsingh2001@gmail.com"),
+                license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")
+        ),
+        servers = @Server(url = "http://localhost:8080", description = "Local Dev Server")
+)
+@SecurityScheme(
+        name = "cookieAuth",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.COOKIE,
+        paramName = "JSESSIONID"
+)
+public class OpenAPIBean {
+}
